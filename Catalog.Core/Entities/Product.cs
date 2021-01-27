3b1953aaf4d31d72343305a1a320f5332701dc88
jlopez15 <jlopez@ecomsur.com>
2021-01-27 19:32:08 -0300

    Added product validations

--- Catalog.Core/Entities/Product.cs
+++ Catalog.Core/Entities/Product.cs
@@ -9,10 +9,14 @@ namespace Catalog.Core.Entities
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "The name field is required.")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "The price field is required.")]
+        [Range(0.0, float.MaxValue, ErrorMessage = "The price value must be greater than 0.")]
         public float Price { get; set; }
 
+        [MaxLength(240, ErrorMessage = "The description length must be minor than 240.")]
         public string Description { get; set; }
 
         public string Image { get; set; }
